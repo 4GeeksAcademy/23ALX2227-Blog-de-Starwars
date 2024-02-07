9f01b14d7db5d9a2bf675ca91412baf6b765b42e
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2024-02-07 23:45:41 +0000

    Logicas

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,37 +1,45 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
+import { StarWarsHome } from "./views/StarWarsHome.jsx";
 import injectContext from "./store/appContext";
-
-import { Home } from "./views/home";
 import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
-import CharactersDetail from "./views/Characters.jsx";
-import VehiclesDetail from "./views/VehiclesDetail.jsx";
+import { Characters } from "./views/Characters.jsx";
+import { People } from "./views/People.jsx";
+import { Vehicles } from "./views/Vehicles.jsx";
+import { VehiclesDetails } from "./views/VehiclesDetails.jsx";
+import { Planets } from "./views/Planets.jsx";
+import { PlanetsDetails } from "./views/PlanetsDetails.jsx";
+
+
+
 
 //create your first component
 const Layout = () => {
-  //the basename is used when your project is published in a subdirectory and not in the root of the domain
-  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-  const basename = process.env.BASENAME || "";
+	//the basename is used when your project is published in a subdirectory and not in the root of the domain
+	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	const basename = process.env.BASENAME || "";
 
-  return (
-    <div>
-      <BrowserRouter basename={basename}>
-        <ScrollToTop>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/character/:id" element={<CharactersDetail />} />
-            <Route path="/vehicle/:id" element={<VehiclesDetail />} />
-            <Route path="*" element={<h1>Not found... May the Force be with you.</h1>} />
-          </Routes>
-          <Footer />
-        </ScrollToTop>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter basename={basename}>
+				<ScrollToTop>
+					<Navbar />
+					<Routes>
+						<Route path="/" element={<StarWarsHome />} />
+						<Route path="/characters" element={<Characters />} />
+						<Route path="/vehicles" element={<Vehicles />} />
+						<Route path="/planets" element={<Planets />} />
+						<Route path="/vehicles/vehiclesdetails/:id" element={<VehiclesDetails />} />
+						<Route path="/characters/people/:id" element={<People />} />
+						<Route path="/planets/planetsdetails/:id" element={<PlanetsDetails />} />
+						<Route path="*" element={<h1>Not found! :(</h1>} />
+					</Routes>
+
+				</ScrollToTop>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default injectContext(Layout);
